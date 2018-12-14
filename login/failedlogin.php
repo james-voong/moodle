f91f459f7c098e97fa66683d311e205791032dbd
James Voong <jamesvoong@catalyst.net.nz>
2018-12-14 14:33:15 +1300

    auth/oauth2 cleaned up the following files: login/failedlogin.php login/redirect_to_idp.php

--- login/failedlogin.php
+++ login/failedlogin.php
@@ -1,30 +1,48 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Display the text "You are logged out" to the user.
-  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-   */
+ * Display the text "Failed to login" to the user.
+ *
+ * @package    core
+ * @subpackage auth
+ * @author     James Voong
+ * @copyright  2018 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-   require_once('../config.php');
+require_once('../config.php');
 
-   $PAGE->set_url('/login/failedlogin.php');
-   $PAGE->set_context(context_system::instance());
+$PAGE->set_url('/login/failedlogin.php');
+$PAGE->set_context(context_system::instance());
 
-   $sesskey = optional_param('sesskey', '__notpresent__', PARAM_RAW); // we want not null default to prevent required sesskey warning
-   $login   = optional_param('loginpage', 0, PARAM_BOOL);
+$sesskey = optional_param('sesskey', '__notpresent__', PARAM_RAW); // we want not null default to prevent required sesskey warning
+$login   = optional_param('loginpage', 0, PARAM_BOOL);
 
-   if (isloggedin()) {
-        // User is not supposed to be here. Send them to the main page.
-            $siteroot = $CFG->wwwroot.'/';
-            var_dump("siteroot is the issue?");die;
-                redirect($siteroot);
-                var_dump("logged in");die;
-   }
-   $PAGE->set_title(get_string('loggedouttitle','auth_oauth2'));
-   $PAGE->set_heading($SITE->fullname);
-   echo $OUTPUT->header();
-   echo $OUTPUT->heading(get_string('youareloggedout','auth_oauth2'));
-   $siteroot = $CFG->wwwroot;
-   $loggedouttext = get_string('loggedoutlong', 'auth_oauth2', $siteroot);
-   echo $OUTPUT->box($loggedouttext, 'generalbox boxaligncenter');
-   echo $OUTPUT->footer();
+if (isloggedin()) {
+    // User is not supposed to be here. Send them to the main page.
+    $siteroot = $CFG->wwwroot.'/';
+    redirect($siteroot);
+}
+
+$PAGE->set_title(get_string('failedlogintitle','auth_oauth2'));
+$PAGE->set_heading($SITE->fullname);
+echo $OUTPUT->header();
+echo $OUTPUT->heading(get_string('faillogintext','auth_oauth2'));
+$siteroot = $CFG->wwwroot;
+$loggedouttext = get_string('tryagainlink', 'auth_oauth2', $siteroot);
+echo $OUTPUT->box($loggedouttext, 'generalbox boxaligncenter');
+echo $OUTPUT->footer();
--- login/redirect_to_idp.php
+++ login/redirect_to_idp.php
@@ -1,12 +1,45 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Login redirect to idp
+ *
+ * @package    core
+ * @subpackage auth
+ * @author     James Voong
+ * @copyright  2018 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once('../config.php');
 
 global $SESSION;
 
 $wantsurl = $SESSION->wantsurl;
+if (empty($SESSION->wantsurl)) {
+    $wantsurl = $CFG->wwwroot;
+}
 
 // id is hardcoded because the point of this is to bounce the user straight to the idp.
 // Seeing as we are bouncing them straight to the idp, we can assume there are no other services.
 // Also, the id is private in the issuer object, so just do the easiest solution.
 // I know, I'm not happy about this either.
-redirect($CFG->wwwroot . '/auth/oauth2/login.php?id=1&wantsurl=' . $wantsurl . '&sesskey=' . sesskey());
+$params = array('id' => 1,
+                'sesskey' => sesskey(),
+                'wantsurl' => $wantsurl);
+
+$url = new moodle_url('/auth/oauth2/login.php', $params);
+redirect($url);
